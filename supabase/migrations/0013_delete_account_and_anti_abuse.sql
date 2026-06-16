c7b932ab12527ae373311a93a3c29577e815f324
fernando <f3228110568@gmail.com>
2026-06-16 09:52:27 -0500

    21

--- supabase/migrations/0013_delete_account_and_anti_abuse.sql
+++ supabase/migrations/0013_delete_account_and_anti_abuse.sql
@@ -139,7 +139,9 @@ security definer
 set search_path = private, public, pg_temp
 as $$
 begin
-  perform private.enforce_rate_limit('anonymous_messages', new.group_id::text, 4, interval '1 minute', interval '8 seconds');
+  if auth.uid() is not null then
+    perform private.enforce_rate_limit('anonymous_messages', new.group_id::text, 4, interval '1 minute', interval '8 seconds');
+  end if;
 
   if char_length(btrim(new.content)) < 1 or char_length(btrim(new.content)) > 500 then
     raise exception 'Invalid anonymous message length';
